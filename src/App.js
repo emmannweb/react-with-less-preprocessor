b6d85dc6bc2f71891d9326b292705260bdcf2d9c
Emmannweb <emmanuelnoc@gmail.com>
2022-08-17 19:10:32 -0300

    [ADDING LESS PREPROCESSOR IN REACT]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.less';
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+          <h1> Heloo Less</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
